4315def6528ccd8098dd540269523255b9e38615
JaydenPears <vladimir120905@gmail.com>
2022-02-22 18:13:10 +0300

    create code

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('Hello World!')
+import os
+import sys
+
+import pygame
+import requests
+
+address = '13.374768,52.518566'
+spn = '0.002,0.002'
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=" + address + "&spn=" + spn + "&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
